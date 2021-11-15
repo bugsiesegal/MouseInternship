9412ec360f17a6538d5ce20d2c41b6dd654bdbc5
bugsiesegal <bugsie@segalnyc.com>
2021-11-15 09:09:05 -0500

    Removed web. Updated Utils.py.

--- Src/Utils.py
+++ Src/Utils.py
@@ -102,3 +102,55 @@ class Fiber_Photometry:
         :return: Fourier transformed data.
         """
         return np.array([np.fft.fft(data) for data in self.data])
+
+    def fft_truncation(self, cutoff):
+        fft = self.fft()
+        self.data = np.fft.ifft(fft[:cutoff])
+
+    def to_window(self, window_size: int = 100) -> windows:
+        """
+        Cuts data into windows with a size of window_size. Data at the end of the array will be truncated if they
+        can't evenly fit.
+
+        :param window_size: The length of each window.
+        :return: Object containing all windows as well as their size.
+        """
+        window_list = []
+        for i in range(self.data.size - window_size * 2):
+            window_list.append(
+                self.data[i + window_size:i + window_size * 2].reshape((1, window_size))
+            )
+
+        return windows(np.array(window_list), window_size)
+
+    @staticmethod
+    def batch_to_window(fp_objects: list[Fiber_Photometry], window_size: int = 100) -> list[windows]:
+        """
+        Cuts uses to_window on list of Fiber_Photometry objects.
+
+        :param fp_objects: List of Fiber_Photometry objects.
+        :param window_size: The length of each window.
+        :return: List of Objects containing all windows of each Fiber_Photometry object and their size.
+        """
+        batch_windows = []
+        for i in fp_objects:
+            batch_windows.append(i.to_window(window_size))
+
+        return batch_windows
+
+
+
+@dataclass
+class windows:
+    data: np.ndarray
+    window_size: int
+
+    def save(self, path):
+        with open(path, 'wb') as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def load(cls, path):
+        with open(path, 'rb') as file:
+            return pickle.load(file)
+
